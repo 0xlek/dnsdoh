a5b5c50b06fe1a490db463775e3f61362e81e815
0xlek <sgamidinov@gmail.com>
2024-06-09 02:35:56 +0400

    add cache layer

--- go.mod
+++ go.mod
@@ -3,3 +3,14 @@ module github.com/0xlek/dnsdoh
 go 1.22.0
 
 require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/miekg/dns v1.1.59 // indirect
+	github.com/redis/go-redis/v9 v9.5.3 // indirect
+	golang.org/x/mod v0.16.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/tools v0.19.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,66 @@
 package main
 
 import (
+	"context"
 	"encoding/base64"
 	"fmt"
 	"github.com/joho/godotenv"
+	"github.com/miekg/dns"
+	"github.com/redis/go-redis/v9"
 	"io"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"time"
 )
+
+type Cache struct {
+	client *redis.Client
+	expiry time.Duration
+}
+
+func (c *Cache) Get(name string) []byte {
+	fmt.Println(name)
+	msg, err := c.client.Get(context.Background(), name).Bytes()
+
+	if err != nil {
+		fmt.Println("miss cache ", name)
+		return nil
+	}
+
+	return msg
+}
+
+func (c *Cache) Set(name string, response *[]byte) {
+	err := c.client.Set(context.Background(), name, *response, c.expiry).Err()
+
+	if err != nil {
+		fmt.Println("failed to cache")
+	}
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	redisAddr := os.Getenv("REDIS_ADDR")
+
+	rdb := redis.NewClient(&redis.Options{
+		Password: "",
+		Addr:     redisAddr,
+		DB:       0,
+	})
+
+	_, err = rdb.Ping(context.Background()).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	cache := Cache{client: rdb, expiry: 15 * time.Minute}
+
 	udpAddress := os.Getenv("ADDRESS")
 	// Listen for incoming UDP packets
 	pc, err := net.ListenPacket("udp", udpAddress)
@@ -23,17 +68,66 @@ func main() {
 		fmt.Printf("Error starting UDP listener: %v\n", err)
 		return
 	}
-	defer pc.Close()
-	fmt.Printf("Listening on %s\n", udpAddress)
 
+	defer pc.Close()
+
+	fmt.Printf("Listening on %s\n", udpAddress)
 	for {
-		handleClient(pc)
+		handleClient(pc, &cache)
 	}
 }
 
-func handleClient(pc net.PacketConn) {
-	buf := make([]byte, 512)
+func GetDnsQuery(msg []byte) string {
+	packet := new(dns.Msg)
+	err := packet.Unpack(msg)
+
+	if err != nil {
+		fmt.Println("failed to unpack buffer to a msg struct")
+		return ""
+	}
+
+	if len(packet.Question) == 0 {
+		return ""
+	}
+
+	question := packet.Question[0]
+	return question.String()
+}
+
+func GetMsgId(msg []byte) uint16 {
+	packet := new(dns.Msg)
+	err := packet.Unpack(msg)
+
+	if err != nil {
+		fmt.Println("failed to unpack message")
+		return 0
+	}
+
+	return packet.Id
+}
+
+func SetMsgId(msg []byte, id uint16) []byte {
+	packet := new(dns.Msg)
+	err := packet.Unpack(msg)
+
+	if err != nil {
+		return msg
+	}
+
+	packet.Id = id
+	packed, err := packet.Pack()
+
+	if err != nil {
+		return msg
+	}
+
+	return packed
+}
+
+func handleClient(pc net.PacketConn, cache *Cache) {
+	buf := make([]byte, 2048)
 	n, addr, err := pc.ReadFrom(buf)
+
 	if err != nil {
 		fmt.Printf("Error reading UDP packet: %v\n", err)
 		return
@@ -41,12 +135,19 @@ func handleClient(pc net.PacketConn) {
 
 	// Decode the DNS query
 	dnsQuery := buf[:n]
+	cacheKey := GetDnsQuery(buf)
+	dohResponse := cache.Get(cacheKey)
 
-	// Forward the DNS query to the DoH server
-	dohResponse, err := queryDoH(dnsQuery)
-	if err != nil {
-		fmt.Printf("Error querying DoH server: %v\n", err)
-		return
+	if dohResponse == nil {
+		// Forward the DNS query to the DoH server
+		dohResponse, err = queryDoH(dnsQuery)
+		cache.Set(cacheKey, &dohResponse)
+		if err != nil {
+			fmt.Printf("Error querying DoH server: %v\n", err)
+			return
+		}
+	} else {
+		dohResponse = SetMsgId(dohResponse, GetMsgId(buf))
 	}
 
 	// Send the DoH response back to the client
@@ -58,26 +159,29 @@ func handleClient(pc net.PacketConn) {
 
 func queryDoH(dnsQuery []byte) ([]byte, error) {
 	dohServerURL := os.Getenv("DOH_URL")
-	fmt.Println("received request for", dnsQuery)
 	// Encode the DNS query in Base64URL format
 	encodedQuery := base64.RawURLEncoding.EncodeToString(dnsQuery)
 
 	// Create the DoH request
 	req, err := http.NewRequest("GET", dohServerURL+"?dns="+encodedQuery, nil)
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to create DoH request: %v", err)
 	}
-	req.Header.Set("Accept", "application/dns-message")
 
+	req.Header.Set("Accept", "application/dns-message")
 	client := &http.Client{}
 	resp, err := client.Do(req)
+
 	if err != nil {
 		return nil, fmt.Errorf("DoH request failed: %v", err)
 	}
+
 	defer resp.Body.Close()
 
 	// Read the DoH response
 	dohResponse, err := io.ReadAll(resp.Body)
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to read DoH response: %v", err)
 	}
